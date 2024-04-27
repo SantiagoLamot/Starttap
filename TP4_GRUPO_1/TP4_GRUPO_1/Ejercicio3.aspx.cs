29a9ea3a94d32c50d4d15ccf8d2ca386691ab142
JennyEchenique <jenneche91@gmail.com>
2024-04-27 12:05:04 -0300

    EJERCICIO 3

---
+++ TP4_GRUPO_1/TP4_GRUPO_1/Ejercicio3.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP4_GRUPO_1
+{
+    public partial class Ejercicio3 : System.Web.UI.Page
+    {
+        private const string cadenaConexionLibreria = "Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True";
+        SqlConnection sqlLibreriaConnection = new SqlConnection(cadenaConexionLibreria);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+
+            {
+
+                SqlConnection sqlConnection = new SqlConnection(cadenaConexionLibreria);
+                sqlLibreriaConnection.Open();
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * from Temas", sqlLibreriaConnection);
+
+                DataSet dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet, "TablaTemas");
+
+                ddltemas.DataSource = dataSet.Tables["TablaTemas"];
+                ddltemas.DataTextField = "Tema";
+                ddltemas.DataBind();
+
+
+                sqlConnection.Close();
+
+            }
+
+        }
+
+        protected void ddltemas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
