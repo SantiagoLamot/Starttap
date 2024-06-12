3339a9d7b69a5176921d2ccd7f56857100ac1f30
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2024-06-12 16:44:25 -0300

    Base de datos

---
+++ TIF-GRUPO_1/bd.sql
@@ -0,0 +1,311 @@
+USE [master]
+GO
+/****** Object:  Database [TIF_ProgramacionIII]    Script Date: 12/6/2024 16:39:58 ******/
+CREATE DATABASE [TIF_ProgramacionIII]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'TIF_ProgramacionIII', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\TIF_ProgramacionIII.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'TIF_ProgramacionIII_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\TIF_ProgramacionIII_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [TIF_ProgramacionIII].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET RECOVERY FULL 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET  MULTI_USER 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'TIF_ProgramacionIII', N'ON'
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [TIF_ProgramacionIII]
+GO
+/****** Object:  Table [dbo].[Categoria]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categoria](
+	[IdCategoria] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nchar](50) NOT NULL,
+ CONSTRAINT [PK_Categoria] PRIMARY KEY CLUSTERED 
+(
+	[IdCategoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Categoria.Producto]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categoria.Producto](
+	[IdCategoria] [int] NOT NULL,
+	[IdProductor] [int] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Empleado]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Empleado](
+	[IdEmpleado] [int] IDENTITY(1,1) NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+	[Salario] [money] NULL,
+	[IdRol] [int] NULL,
+	[FechaIngreso] [datetime] NULL,
+	[Horarios] [varchar](20) NULL,
+	[Estado] [bit] NULL,
+ CONSTRAINT [PK_Empleado] PRIMARY KEY CLUSTERED 
+(
+	[IdEmpleado] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Mesa]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Mesa](
+	[IdMesa] [int] IDENTITY(1,1) NOT NULL,
+	[Numero] [int] NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+	[DeudaTotal] [money] NOT NULL,
+ CONSTRAINT [PK_Mesa] PRIMARY KEY CLUSTERED 
+(
+	[IdMesa] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Ordenes]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Ordenes](
+	[IdOrden] [int] IDENTITY(1,1) NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+	[IdEmpleado] [int] NOT NULL,
+	[IdMesa] [int] NOT NULL,
+	[Fecha] [datetime] NOT NULL,
+	[Total] [money] NOT NULL,
+	[EstadoComanda] [bit] NOT NULL,
+	[EstadoPreparacion] [bit] NOT NULL,
+ CONSTRAINT [PK_Ordenes] PRIMARY KEY CLUSTERED 
+(
+	[IdOrden] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Productos]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Productos](
+	[IdProducto] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nchar](50) NOT NULL,
+	[Precio] [money] NOT NULL,
+	[IdCategoria] [int] NOT NULL,
+	[Stock] [int] NOT NULL,
+	[Estado] [bit] NOT NULL,
+ CONSTRAINT [PK_Productos] PRIMARY KEY CLUSTERED 
+(
+	[IdProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Productos.Orden]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Productos.Orden](
+	[IdDetalle] [int] IDENTITY(1,1) NOT NULL,
+	[IdOrden] [int] NOT NULL,
+	[IdProducto] [int] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[Subtotal] [money] NOT NULL,
+ CONSTRAINT [PK_Productos.Orden] PRIMARY KEY CLUSTERED 
+(
+	[IdDetalle] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Reserva]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Reserva](
+	[IdReserva] [int] IDENTITY(1,1) NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+	[IdMesa] [int] NOT NULL,
+	[Fecha] [datetime] NOT NULL,
+	[Estado] [bit] NOT NULL,
+ CONSTRAINT [PK_Reserva] PRIMARY KEY CLUSTERED 
+(
+	[IdReserva] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[IdRol] [int] IDENTITY(1,1) NOT NULL,
+	[Cargo] [nchar](20) NOT NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[IdRol] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuario]    Script Date: 12/6/2024 16:39:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[IdUsuario] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nchar](50) NOT NULL,
+	[Apellido] [nchar](50) NOT NULL,
+	[Email] [nchar](50) NOT NULL,
+	[Contraseña] [nchar](50) NOT NULL,
+	[TipoUsuario] [int] NOT NULL,
+	[DNI] [nchar](20) NOT NULL,
+	[Telefono] [nchar](20) NOT NULL,
+	[Direccion] [nchar](50) NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[IdUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Categoria.Producto]  WITH CHECK ADD  CONSTRAINT [FK_Categoria.Producto_Categoria] FOREIGN KEY([IdCategoria])
+REFERENCES [dbo].[Categoria] ([IdCategoria])
+GO
+ALTER TABLE [dbo].[Categoria.Producto] CHECK CONSTRAINT [FK_Categoria.Producto_Categoria]
+GO
+ALTER TABLE [dbo].[Categoria.Producto]  WITH CHECK ADD  CONSTRAINT [FK_Categoria.Producto_Productos] FOREIGN KEY([IdProductor])
+REFERENCES [dbo].[Productos] ([IdProducto])
+GO
+ALTER TABLE [dbo].[Categoria.Producto] CHECK CONSTRAINT [FK_Categoria.Producto_Productos]
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Roles] FOREIGN KEY([IdRol])
+REFERENCES [dbo].[Roles] ([IdRol])
+GO
+ALTER TABLE [dbo].[Empleado] CHECK CONSTRAINT [FK_Empleado_Roles]
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Usuario] FOREIGN KEY([IdUsuario])
+REFERENCES [dbo].[Usuario] ([IdUsuario])
+GO
+ALTER TABLE [dbo].[Empleado] CHECK CONSTRAINT [FK_Empleado_Usuario]
+GO
+ALTER TABLE [dbo].[Ordenes]  WITH CHECK ADD  CONSTRAINT [FK_Ordenes_Empleado] FOREIGN KEY([IdEmpleado])
+REFERENCES [dbo].[Empleado] ([IdEmpleado])
+GO
+ALTER TABLE [dbo].[Ordenes] CHECK CONSTRAINT [FK_Ordenes_Empleado]
+GO
+ALTER TABLE [dbo].[Ordenes]  WITH CHECK ADD  CONSTRAINT [FK_Ordenes_Mesa] FOREIGN KEY([IdMesa])
+REFERENCES [dbo].[Mesa] ([IdMesa])
+GO
+ALTER TABLE [dbo].[Ordenes] CHECK CONSTRAINT [FK_Ordenes_Mesa]
+GO
+ALTER TABLE [dbo].[Ordenes]  WITH CHECK ADD  CONSTRAINT [FK_Ordenes_Usuario] FOREIGN KEY([IdUsuario])
+REFERENCES [dbo].[Usuario] ([IdUsuario])
+GO
+ALTER TABLE [dbo].[Ordenes] CHECK CONSTRAINT [FK_Ordenes_Usuario]
+GO
+ALTER TABLE [dbo].[Productos.Orden]  WITH CHECK ADD  CONSTRAINT [FK_Productos.Orden_Ordenes] FOREIGN KEY([IdOrden])
+REFERENCES [dbo].[Ordenes] ([IdOrden])
+GO
+ALTER TABLE [dbo].[Productos.Orden] CHECK CONSTRAINT [FK_Productos.Orden_Ordenes]
+GO
+ALTER TABLE [dbo].[Productos.Orden]  WITH CHECK ADD  CONSTRAINT [FK_Productos.Orden_Productos] FOREIGN KEY([IdProducto])
+REFERENCES [dbo].[Productos] ([IdProducto])
+GO
+ALTER TABLE [dbo].[Productos.Orden] CHECK CONSTRAINT [FK_Productos.Orden_Productos]
+GO
+ALTER TABLE [dbo].[Reserva]  WITH CHECK ADD  CONSTRAINT [FK_Reserva_Mesa] FOREIGN KEY([IdMesa])
+REFERENCES [dbo].[Mesa] ([IdMesa])
+GO
+ALTER TABLE [dbo].[Reserva] CHECK CONSTRAINT [FK_Reserva_Mesa]
+GO
+ALTER TABLE [dbo].[Reserva]  WITH CHECK ADD  CONSTRAINT [FK_Reserva_Usuario] FOREIGN KEY([IdUsuario])
+REFERENCES [dbo].[Usuario] ([IdUsuario])
+GO
+ALTER TABLE [dbo].[Reserva] CHECK CONSTRAINT [FK_Reserva_Usuario]
+GO
+USE [master]
+GO
+ALTER DATABASE [TIF_ProgramacionIII] SET  READ_WRITE 
+GO
