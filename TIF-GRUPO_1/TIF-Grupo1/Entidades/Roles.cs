54da35b02f92d23d3e8ec3defebd7cb63ce17405
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2024-06-13 23:23:33 -0300

    Capas y Entidades
    Agregue las capas Negocio, Datos y Entidades, con su referencias y también  cree la clase Empleado y Roles.

---
+++ TIF-GRUPO_1/TIF-Grupo1/Entidades/Roles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Roles
+    {
+        private int _IdRol;
+        private string _Cargo;
+
+        public Roles() { }
+
+        public Roles(int IdRol, string Cargo) 
+        {
+            _IdRol = IdRol;
+            _Cargo = Cargo;
+        }
+        public int IdRol
+        {
+            get { return _IdRol; }
+            set { _IdRol = value; }
+        }
+        public string Cargo
+        {
+            get { return _Cargo; }
+            set { _Cargo = value; }
+        }
+    }
+}
