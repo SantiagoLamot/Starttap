f8c124635bfbe325d267f2108c10de89f46015a1
JennyEchenique <jenneche91@gmail.com>
2024-06-15 17:43:22 -0300

    clases Categoria y Producto
    en la Capa entedidas se creolas clases Categoria y Producto, cada una de ellas con los atributos, constructores y metodos Get y Set correspondiente.

---
+++ TIF-GRUPO_1/TIF-Grupo1/Entidades/Categoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        private int _idCategoria;
+        private string _Nombre; 
+
+        public Categoria() { }
+
+        public Categoria (int idCategoria, string nombre) 
+        {
+            _idCategoria = idCategoria;
+            _Nombre = nombre;
+        }
+
+        public int idCategoria {
+            get { return _idCategoria;} 
+            set { _idCategoria = value; } 
+        }
+
+        public string nombre { 
+            get { return _Nombre;} 
+            set { _Nombre = value; } 
+        }
+    }
+
+}
---
+++ TIF-GRUPO_1/TIF-Grupo1/Entidades/Producto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private int _idProducto;
+        private string _Nombre;
+        private decimal _Precio;
+        private Categoria _Categoria;
+        private int _Stock;
+        private bool _Estado;
+
+        public Producto() { }
+
+        public Producto (int idProducto, string nombre, decimal precio, Categoria categoria, int stock, bool estado)
+        {
+            _idProducto = idProducto;
+            _Nombre = nombre;
+            _Precio = precio;
+            _Categoria = categoria;
+            _Stock = stock;
+            _Estado = estado;
+        }
+
+        public int idProducto { 
+            get { return _idProducto; } 
+            set { _idProducto = value; } 
+        }
+
+        public string nombre { 
+            get { return _Nombre; } 
+            set { _Nombre = value; }
+        }
+
+        public decimal precio { 
+            get { return _Precio; } 
+            set { _Precio = value; }
+        }
+
+        public Categoria categoria {
+            get { return _Categoria; } 
+            set { _Categoria = value; }
+        }
+
+        public int stock { 
+            get { return _Stock; } 
+            set {  _Stock = value; }
+        }
+
+        public bool estado { 
+            get { return _Estado; } 
+            set { _Estado = value; }
+        }
+    }   
+    
+}
